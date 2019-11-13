147ac79d5d7fb85f1757515fef2e0ff546948cb6
Kevin Montero <kevinwilding1997@gmail.com>
2019-11-12 19:16:59 -0700

    Assignment DOne

---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+ class FilterString extends Component {
+     constructor(props) {
+         super(props);
+         this.state = {  };
+     }
+     render() {
+         return (
+             <div></div>
+         );
+     }
+ }
+ 
+ export default FilterString;
